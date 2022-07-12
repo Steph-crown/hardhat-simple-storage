b547accdb6d92b9702613f9daff20e6913fcdd55
steph-crown <emmanuelstephen024@gmail.com>
2022-07-12 08:47:38 +0100

    Add verification logic

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
+const GEORLI_RPC_URL = process.env.GEORLI_RPC_URL;
+const GEORLI_PRIVATE_KEY = process.env.GEORLI_PRIVATE_KEY;
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -15,5 +21,14 @@ module.exports = {
       // check chain ids from chainlist.org
       chainId: 4,
     },
+    georli: {
+      url: GEORLI_RPC_URL,
+      accounts: [GEORLI_PRIVATE_KEY],
+      // check chain ids from chainlist.org
+      chainId: 5,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,44 @@
 // imports
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
+// run allows us run any hardhat tag with parameters
+// to get network config info
 
 // async main
 async function main() {
   const SimpleStorageContractFactory = await ethers.getContractFactory(
     "SimpleStorage"
   );
+  console.log(network);
 
   console.log("Deploying contract...");
   const simpleStorage = await SimpleStorageContractFactory.deploy();
 
   await simpleStorage.deployed();
   console.log("SimpleStorage contract deployed at:", simpleStorage.address);
+
+  // check chain id
+  // if chain id is not hardhat chain id and etherscan api key exists
+  if (network.config.chainId !== 31337 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6); //wait 6 blocks
+    await verify(simpleStorage.address, []);
+  }
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+    console.log("verified");
+  } catch (err) {
+    if (err.message.toLowerCase().includes("already verified")) {
+      console.error("Already verified");
+    } else {
+      console.log(err);
+    }
+  }
 }
 
 // main
